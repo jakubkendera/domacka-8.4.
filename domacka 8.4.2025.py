657d8c391d70406316e9ae504fe2d9f1836e8e2a
jakubkendera <180315366+jakubkendera@users.noreply.github.com>
2025-04-07 16:38:47 +0200

    Add files via upload

---
+++ domacka 8.4.2025.py
@@ -0,0 +1,14 @@
+fr=open("ciernobiely_obrazok_1.txt","r",encoding="UTF-8")
+riadok=fr.readline() #zoberieme iba prvy riadok
+sirka, vyska=riadok.strip().split()
+sirka=int(sirka)
+vyska=int(vyska)
+
+obrazok=Image.new("RGB",(sirka,vyska),(255,255,255))
+pixels=obrazok.load()
+pixels[10,10]=(0,0,0)
+pixels[11,10]=(0,0,0)
+pixels[12,10]=(0,0,0)
+
+obrazok.save("ciernobielyObrazok.png")
+obrazok.show()
